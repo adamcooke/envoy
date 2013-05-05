8bfa4dc4961accfefdd22737d6fc8035dfb1a648
Nathan Baum <n@p12a.org.uk>
2013-05-05 20:15:52 +0100

    Allow web listening address to be configured

--- lib/envoy/server/command.rb
+++ lib/envoy/server/command.rb
@@ -1,9 +1,17 @@
 require 'envoy/server/trunk'
 require 'envoy/server/web'
+require 'optparse'
 
-unless ARGV[0]
-  puts "Usage: #{$0} [ZONE]"
-  abort
+listen = []
+
+OptionParser.new do |op|
+  op.banner = "Usage: #{$0} [options] ZONE"
+  op.on "-l", "--listen ADDRESS", "Listen on this [host:]port for HTTP" do |v|
+    port, host = v.split(":").reverse
+    listen = [host || "0.0.0", port]
+  end
+  op.parse!
+  op.abort "zone required" unless ARGV[0]
 end
 
 $zone = ARGV[0]
@@ -11,7 +19,7 @@ $zone = ARGV[0]
 unless EM.reactor_running?
   EM.run do
     EM.start_server "0.0.0.0", 8282, Envoy::Server::Trunk
-    EM.start_server "0.0.0.0", 8181, Envoy::Server::Web
+    EM.start_server *listen, Envoy::Server::Web
   end
 end
 
